646aeb016269c3ab504669d7571fd98eef05c856
RoseBarrow <34482704+RoseBarrow@users.noreply.github.com>
2018-02-16 09:07:07 -0700

    Add files via upload

---
+++ Day 1 exercise 1.py
@@ -0,0 +1,13 @@
+#Christine Barrow - Python Daily Exercise - Day 1 EX: 1 - 02162018
+
+# 1. Write a statement that concatenates at least two variables (you can have more than 2 for this practice).
+
+name = "Christine"
+age = 17
+
+print ("Hi my name is " + name + " i am" + str(age))
+
+
+
+
+
---
+++ Day 1 exercise 2 .py
@@ -0,0 +1,14 @@
+#Christine Barrow - Python Daily Exercise - Day 1 EX: 2 - 02162018
+
+# 2. Create a program that asks the user to enter their name and their age. 
+# Print out a message addressed to them that tells them the year that they will turn 100 years old. Clue input()
+
+name = input('Enter name: ')
+years = 100
+age = int(input('Enter age: '))
+
+print("Hello " + name + " in " + str(years - age) + " years you will be 100 years old.")
+
+
+
+
